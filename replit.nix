13d56d0f719d86153bd4c5d8492b8c262ef0e2f6
Angel Ramirez <angel-krak@hotmail.com>
2023-08-19 21:09:58 +0000

    Primer commit

--- replit.nix
+++ replit.nix
@@ -1,6 +1,8 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-18_x
+		pkgs.zip
+  pkgs.sudo
+  pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
     pkgs.replitPackages.jest
