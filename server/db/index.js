3a710a13b36f7e6e0fa05d3be9f7a7f58448967a
AngelKrak <angel-krak@hotmail.com>
2023-08-19 22:30:23 -0600

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,16 @@ const config = require("../config");
 const { mongodbUrl } = config;
 const connect = async () => {
   try {
-    await mongoose.connect(mongodbUrl);
-    console.log("mongo db connected");
-  } catch (error) {
-    console.log(error.message);
-  }
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(mongodbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error("error: ", err.message);
+        // make the process fail
+        process.exit(1);
+    }
 };
 module.exports = connect;
