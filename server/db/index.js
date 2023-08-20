405f48ccab88d29e6380aedbf68698b8841b2a64
AngelKrak <angel-krak@hotmail.com>
2023-08-19 22:32:18 -0600

    1 file modified, 1 added

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
-const config = require("../config");
+const mongoose = require('mongoose');
+const config = require('../config');
 const { mongodbUrl } = config;
 const connect = async () => {
   try {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(mongodbUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error("error: ", err.message);
-        // make the process fail
-        process.exit(1);
-    }
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(mongodbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('error: ', err.message);
+    // make the process fail
+    process.exit(1);
+  }
 };
 module.exports = connect;
