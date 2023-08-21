cfb252d3b7e02e6584763cd6bb209f80a49af822
AngelKrak <angel-krak@hotmail.com>
2023-08-20 19:12:29 -0600

    3 files modified

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const compression = require("compression");
-const config = require("./config");
-const mongodbConnection = require("./db");
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const config = require('./config');
+const mongodbConnection = require('./db');
 
-const postRoutes = require("./routes/post.routes");
-const sseRoute = require("./routes/sse.route");
+const postRoutes = require('./routes/post.routes');
+const sseRoute = require('./routes/sse.route');
 
 const { port, origin } = config;
 const app = express();
@@ -21,19 +21,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(sseRoute);
-app.use("/api", postRoutes);
-app.get("/", (req, res) => {
-	res.status(200).json({
-		status: true,
-		message: "El servidor esta funcionando sin problemas."
-	});
+app.use('/api', postRoutes);
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: 'El servidor esta funcionando sin problemas.',
+  });
 });
 
 const startServer = () => {
-  app.listen(port, '0.0.0.0', () => {
+  app.listen(port || 5000, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${port}`);
   });
-}
+};
 
 (async () => {
   try {
